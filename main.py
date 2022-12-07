79765fee0720fe8e3144ae1814ea101cfa465cce
Gagan Sharma <80513417+DemiwolfLegend@users.noreply.github.com>
2022-12-07 07:24:35 +0530

    loading player

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ screen = pygame.display.set_mode((800, 800))
 backgroundImage = pygame.image.load("./content/background.png")
 
 
+class character:
+    def __init__(self) -> None:
+        self.image = pygame.image.load("./content/player.png")
+        self.x = 350
+        self.y = 700
+        self.change = 0
+
+    def show(self):
+        screen.blit(self.image, (self.x, self.y))
+
+
+player = character()
 run = True
 while run:
     screen.blit(backgroundImage, (0, 0))
@@ -13,4 +25,5 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    player.show()
     pygame.display.update()
