2e1efa1db514fdcaad7e04bf9f956dcbd6b76d86
Gagan Sharma <80513417+DemiwolfLegend@users.noreply.github.com>
2022-12-07 07:07:38 +0530

    set background

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 
-screen = pygame.display.set_mode((800, 800))
-
 pygame.display.set_caption("firstGame")
+
+screen = pygame.display.set_mode((800, 800))
+backgroundImage = pygame.image.load("./content/background.png")
+
 
 run = True
 while run:
+    screen.blit(backgroundImage, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
